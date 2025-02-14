21ee72f58b69fbc954daa9bf29045079e2f611df
BHAVANIBOJEDLA <bhavanibojadla8@gmail.com>
2025-02-14 12:28:33 +0530

    resolved mongodb

--- Connection/conn.js
+++ Connection/conn.js
@@ -2,15 +2,13 @@ const mongoose=require('mongoose');
 
 const dbconnect=async ()=>{
       try{
-        await mongoose.connect("mongodb+srv://admin:T87Ku3cJmHZ8JX0p@cluster0.zbicutx.mongodb.net/Todo")
+        await mongoose.connect("mongodb+srv://admin:Py6CUFdbc2Rs7H1B@cluster0.zbicutx.mongodb.net/Todo")
         .then(()=>{
           console.log("connected to db")
         })
       }
       catch(e){
-           res.status(400).json({
-            msg:"not connected"
-           })
+           console.log(e)
       }
 }
 
